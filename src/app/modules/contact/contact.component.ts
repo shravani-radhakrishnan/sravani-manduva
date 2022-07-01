fd2b4aa967011628ae434573a71beaa52d9bf85d
shravani-radhakrishnan <manduvasravani@gmail.com>
2022-07-01 06:27:17 -0500

    contact form changes

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-contact',
@@ -7,24 +8,56 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  submitted = false;
   contactForm = new FormGroup({
     firstname: new FormControl('',[Validators.required,Validators.minLength(2)]),
     lastname: new FormControl('',[Validators.required,Validators.minLength(2)]),
     email: new FormControl('',[Validators.required,Validators.email]),
     mobile: new FormControl('',[Validators.pattern("^((\\+1-?)|0)?[0-9]{10}$")]),
-    message: new FormControl('',[Validators.maxLength(300),Validators.minLength(10)]),
-    honeypot: new FormControl()
+    message: new FormControl('',[Validators.maxLength(300),Validators.minLength(3)]),
+    // honeypot: new FormControl()
   })
   constructor() { }
 
   ngOnInit(): void {
   }
+  // This function is used to submit the form values
+  onSubmit(){
+    this.submitted = true;
+    let invalidControls = this.findInvalidControls()
+    console.log(invalidControls)
+    if (this.contactForm.invalid) {
+      // for(let i=0;i<invalidControls?.length;i++){
 
-  onSubmit() {
-    console.log(this.contactForm.value);
+      // }
+     return
+    } else {
+      console.log('form submitted');
+      $('button p').text(function(i, text) {
+        $('#clickMe').toggleClass('clicked');
+        return text === "Sent!" ? "Send" : "Sent!";
+      });
+    }
+  }
+
+  findInvalidControls() {
+    const invalid = [];
+    const controls = this.contactForm.controls;
+    for (const name in controls) {
+        if (controls[name].invalid) {
+            invalid.push(name);
+        }
+    }
+    return invalid;
+}
+  // This function is used to reset all from values
+  onReset() {
+    this.submitted = false;
+    this.contactForm.reset();
+  }
+    
     // if(this.contactForm.status =="VALID" && this.honeypot.value ==""){
     //   this.contactForm.disable() //Disable multiple submissions
     // }
-  }
 
 }
